5f13fbb0e849f449d3b9c21fb6c2a813ff8189a7
WailBenoulha <wailbnlh@gmail.com>
2024-09-22 15:45:30 -1100

    add the all the swagger config

--- app/app/settings.py
+++ app/app/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'ecomm',
     'user',
     'rest_framework',
+    'drf_spectacular',
+    'drf_spectacular_sidecar', 
 ]
 
 MIDDLEWARE = [
@@ -131,3 +133,24 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.CustomUser'
+
+
+REST_FRAMEWORK = {
+    # YOUR SETTINGS
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Your Project API',
+    'DESCRIPTION': 'Your project description',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+    # OTHER SETTINGS
+}
+
+SPECTACULAR_SETTINGS = {
+    'SWAGGER_UI_DIST': 'SIDECAR',  # shorthand to use the sidecar instead
+    'SWAGGER_UI_FAVICON_HREF': 'SIDECAR',
+    'REDOC_DIST': 'SIDECAR',
+    # OTHER SETTINGS
+}
